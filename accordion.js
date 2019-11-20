0d14ba55588bf870994b2d75b731062d1fa07485
ShaughnW <swb_92@live.co.uk>
2019-11-20 22:18:12 +0000

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,38 +1,12 @@
-.accordion {
-  margin-bottom: 4rem;
-}
+$(document).ready(function() {
 
-.accordion--header {
-    border-bottom: 1px solid #7E7E7E;
-    cursor: pointer;
-    padding: 2rem 0;
-}
+    $(".accordion .accordion--header:first").addClass(".accordion__active").next().slideToggle(); // by default, the first accordion header is active
 
-.accordion--header:hover {
-    background: #F6F7F9;
-}
+    $(".accordion .accordion--header").click(function() { // check if an accordion-header is clicked
 
-.accordion--header.accordion__active {
-  font-weight: 700;
-}
-.accordion--content {
-    display: none;
-    border-bottom: 1px solid rgba(0,0,0,.025);
-    background: rgba(0,0,0,.025);
-    padding: 2rem 2rem 4rem;
-}
-
-.accordion--header::before {
-    content: '';
-    vertical-align: middle;
-    display: inline-block;
-    width: .75rem;
-    height: .75rem;
-    border-radius: 50%;
-    background-color: #B1B5BE;
-    margin-right: .75rem;
-}
-
-.accordion--header.accordion__active::before {
-    background-color: #FC6468;
-}
+        $(this).next().slideToggle(); // close this accordion and open others
+        $(".accordion--content").not($(this).next()).slideUp(); // close other accordions but not this one
+        $(this).toggleClass("accordion__active"); // toggle the active class on this accordion, if it's active, it will not be active, if it's not active, it will be active
+        $(".accordion .accordion--header").not($(this)).removeClass("accordion__active"); // other accordions that aren't this one will be made lose the class "active"
+    });
+});
